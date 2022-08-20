5de172c833521670781889e342bc799de0f7fd48
amine-mihoubi <m.mihoubi@univ-batna2.dz>
2022-08-20 23:57:54 +0100

    added html to api

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request
-
+from flask import Flask, jsonify, request, url_for, render_template
+from markupsafe import escape
 # Intitialise the app
 app = Flask(__name__)
 
 # Define what the app does
-@app.get("/greet")
+@app.get("/groot")
 def index():
     """
     TODO:
@@ -14,4 +14,36 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+
+    name = escape(request.args.get( "name" ))
+    last_name = escape(request.args.get( "last_name"))
+    if (not name) and (not last_name):
+        return jsonify({ "status" : "error" })
+    elif (not name) and last_name:
+        return jsonify({ "data" : "Hello, Mr {}!".format(last_name.capitalize()) })
+    elif name and (not last_name):
+        return jsonify({ "data" : "Hello, {}!".format(name.capitalize()) })
+    else:
+        return jsonify({ "data" : "Is your name {} {}?".format(name.capitalize(), last_name.capitalize()) })
+
+@app.get("/")
+def home():
+    name = request.args.get( "name" )
+    last_name = request.args.get( "last_name")
+    if (not name) and (not last_name):
+        color = "#e44100;"
+        response = { "data" : "error" , "color" : color }
+    elif (not name) and last_name:
+        color = "#00a0bd;"
+        response = { "data" : "Hello, Mr {}!".format(last_name.capitalize()), "color" : color }
+    elif name and (not last_name):
+        color = "#00e457;"
+        response = { "data" : "Hello, {}!".format(name.capitalize()), "color" : color }
+    else:
+        color = "#e4af00;"
+        response = { "data" : "Is your name {} {}?".format(name.capitalize(), last_name.capitalize()), "color" : color }
+        print(response['data'])
+    return render_template("index.html",response=response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
