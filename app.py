7f4e4dadcfa9fed6c11621ac65496d5ff2df5447
amine-mihoubi <m.mihoubi@univ-batna2.dz>
2022-08-21 03:03:13 +0100

    added broken get lucky functionality to api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect
-
+from markupsafe import escape
 app = Flask(__name__)
 
 @app.get("/")
@@ -7,16 +7,25 @@ def index():
     """
     TODO: Render the home page provided under templates/index.html in the repository
     """
-    return "TODO"
+    return render_template("index.html")
 
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+    q = escape(request.args.get("q"))
+    """
+    TODO:
+    1. Capture the word that is being searched
+    2. Seach for the word on Google and display results
+    """
+    print(request.form)
+    return redirect("https://www.google.com/search?q=" + q)
+
+@app.get("/lucky")
+def lucky():
+    q = escape(request.args.get("q"))
+    print(q)
+    return redirect("https://www.google.com/search?btnI=1&q={}".format(q))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
