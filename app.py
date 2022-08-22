e5f96770a42571c277810afc997fa526a47d77ae
amine-mihoubi <m.mihoubi@univ-batna2.dz>
2022-08-22 05:01:57 +0100

    added custom filters functionality

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, request, send_file, jsonify
-from bin.filters import apply_filter
+from bin.filters import apply_filter, apply_custom_filter
 
 app = Flask(__name__)
 
 # Read the PIL document to find out which filters are available out-of the box
-filters_available = []
+filters_available = ["blur" ,
+                    "contour" ,
+                    "detail" ,
+                    "edge_enhance" ,
+                    "edge_enhance_more" ,
+                    "emboss" ,
+                    "find_edges" ,
+                    "sharpen" ,
+                    "smooth" ,
+                    "smooth_more" ,
+                    "MYFILTER"
+                    ]
+custom_filters = ["sepia", "black_and_white", "invert"]
+
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -13,7 +26,10 @@ def index():
     TODO:
     1. Return the usage instructions that specifies which filters are available, and the method format
     """
-    pass
+    response = {"filters_available": filters_available+custom_filters,
+                "usage": {"method": "POST", "url": "/<filter_available>"}
+                }
+    return jsonify(response)
 
 
 @app.post("/<filter>")
@@ -26,7 +42,17 @@ def image_filter(filter):
     4. Return the filtered image as response
     """
     pass
+    if (filter not in filters_available) and (filter not in custom_filters):
+        return jsonify({"error": "Filter not available"})
 
+    file = request.files["image"]
+    if not file: 
+        return jsonify({"error": "No file provided"})
+    if filter in filters_available:
+        filtered_image = apply_filter(file, filter)
+    elif filter in custom_filters:
+        filtered_image = apply_custom_filter(file, filter)
+    return send_file(filtered_image, mimetype="image/jpeg")
 
 if __name__ == "__main__":
     app.run()
--- bin/filters.py
+++ bin/filters.py
@@ -1,5 +1,10 @@
+from unicodedata import name
 from PIL import Image, ImageFilter
 import io
+
+# inherite from ImageFilter module
+
+    
 
 
 def apply_filter(file: object, filter: str) -> object:
@@ -11,5 +16,79 @@ def apply_filter(file: object, filter: str) -> object:
     4. Convert the PIL Image object to file object
     5. Return the file object
     """
+    image = Image.open(file)
+    filtered_image = image.filter(getattr(ImageFilter, filter.upper()))
+    output = io.BytesIO()
+    filtered_image.save(output, format="JPEG")
+    output.seek(0)
+    return output
 
-    pass
+def apply_custom_filter(file: object, filter: str) -> object:
+    return eval("{}({})".format(filter,str(file).split(" ")[1]))
+
+def sepia(image_path : str)-> Image:
+    img = Image.open(image_path)
+    width, height = img.size
+    pixels = img.load()
+
+    for py in range(height):
+        for px in range(width):
+            r, g, b = pixels[px, py]
+            tr = int(r * 0.393 + g * 0.769 + b * 0.189) 
+            tg = int(r * 0.349 + g * 0.686 + b * 0.168)
+            tb = int(r * 0.272 + g * 0.534 + b * 0.131)
+            if tr > 255:
+                tr = 255
+            if tg > 255:
+                tg = 255
+            if tb > 255:
+                tb = 255
+
+            pixels[px, py] = (tr, tg, tb)
+
+    output = io.BytesIO()
+    img.save(output, format="JPEG")
+    output.seek(0)
+    return output
+
+def black_and_white(image_path : str)-> Image:
+    img = Image.open(image_path)
+    width, height = img.size
+    pixels = img.load()
+
+
+    for py in range(height):
+        for px in range(width):
+            r, g, b = pixels[px, py]
+            tr = int((r + g + b) / 3)
+            tg = int((r + g + b) / 3)
+            tb = int((r + g + b) / 3)
+            pixels[px, py] = (tr, tg, tb)
+
+
+    output = io.BytesIO()
+    img.save(output, format="JPEG")
+    output.seek(0)
+    return output
+
+def invert(image_path : str)-> Image:
+    img = Image.open(image_path)
+    width, height = img.size
+    pixels = img.load()
+
+
+    for py in range(height):
+        for px in range(width):
+            r, g, b = pixels[px, py]
+            tr = 255 - r
+            tg = 255 - g
+            tb = 255 - b
+            pixels[px, py] = (tr, tg, tb)
+
+
+    output = io.BytesIO()
+    img.save(output, format="JPEG")
+    output.seek(0)
+    return output
+
+
